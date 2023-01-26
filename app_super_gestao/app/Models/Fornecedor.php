dd7e9c6c1305bef02758b1543f5a02bc745c27d7
Caio <caioferreiramendes@gmail.com>
2023-01-26 09:27:11 -0300

    atualização layout

--- app_super_gestao/app/Models/Fornecedor.php
+++ app_super_gestao/app/Models/Fornecedor.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Fornecedor extends Model
 {
     use HasFactory;
+    protected $table = 'fornecedores';
 }
--- app_super_gestao/app/Models/SiteContato.php
+++ app_super_gestao/app/Models/SiteContato.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class SiteContato extends Model
 {
-    use HasFactory;
+    use HasFactory;    
 }
